d88a2e3dc03553d1f87e57ca230ea8151c37a23e
Ardalan Samimi <ardalan.samimi@gmail.com>
2018-06-08 21:01:51 +0200

    Added method to generate main.swift and test files [skip ci]

--- Sources/HazelCore/Support/Language/LanguageSwift.swift
+++ Sources/HazelCore/Support/Language/LanguageSwift.swift
@@ -10,4 +10,15 @@ final class LanguageSwift: Generator {
 		self.defaultFiles = ["Package.swift", ".editorconfig"]
 	}
 
+	override func initProject() throws {
+		try super.initProject()
+		self.generateSwiftFiles()
+	}
+
+	private func generateSwiftFiles() {
+		self.fileManager.createFile(atPath: "Sources/\(self.projectName)/main.swift", contents: nil)
+		self.fileManager.createFile(atPath: "Tests/\(self.projectName)Tests/\(self.projectName)Tests.swift", contents: nil)
+		self.fileManager.createFile(atPath: "Tests/\(self.projectName)Tests/XCTestManifests.swift", contents: nil)
+	}
+
 }
