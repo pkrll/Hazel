042639b211f6710f971923855b609e4cdbdf585c
Ardalan Samimi <ardalan.samimi@bontouch.com>
2019-08-10 23:56:03 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
 	name: "Hazel",
 	dependencies: [
 		.package(url: "https://github.com/onevcat/Rainbow", from: "3.0.0"),
-		.package(url: "https://github.com/pkrll/SwiftArgs", from: "0.5.0")
+		.package(url: "https://github.com/pkrll/SwiftArgs", from: "0.5.5")
 	],
 	targets: [
 		.target(
